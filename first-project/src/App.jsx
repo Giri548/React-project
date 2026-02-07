3578cd6291824cceb5bd8f18b89423b9befe7b4a
Giri548 <skgiri427@gmail.com>
2026-02-07 16:26:25 +0530

    remove

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -8,7 +8,7 @@ import Three from "./assets/three.jpg";
 function App() {
   return (
     <>
-      <Course name="Sivan Frame1" price="$100" img={One} show={true} rating={4.5} />
+      <Course name="Sivan Frame1" price="$100" img={One} show={ true} rating={4.5} />
       <Course name="Sivan Frame2" price="$200" img={Two} show={true} rating={4.2} />
       <Course name="Sivan Frame3" price="$300" img={Three} show={true} rating={4.8} />
     </>
